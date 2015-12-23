9fc5b3467d2d37d199cffb4096463e8e5359f23a
Ryan Pliske <ryan.pliske@monsanto.com>
2015-12-22 19:57:43 -0600

    Added Projectiles

--- spriteKitSpike/GameScene.swift
+++ spriteKitSpike/GameScene.swift
@@ -16,6 +16,35 @@ class GameScene: SKScene {
             ))
     }
     
+    override func touchesEnded(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        guard let touch = touches.first else {
+            return
+        }
+        
+        let touchLocation = touch.locationInNode(self)
+        let projectile = SKSpriteNode(imageNamed: "projectile")
+        projectile.position = player.position
+        
+        guard let offset = projectileOffsetFor(touchLocation, projectilePosition: projectile.position) else {
+            return
+        }
+        
+        addChild(projectile)
+        
+        let direction = offset.normalized()
+        let shootAmount = direction * 1000
+        let destination = shootAmount + projectile.position
+        
+        let actionMove = SKAction.moveTo(destination, duration: 2.0)
+        let actionMoveDone = SKAction.removeFromParent()
+        projectile.runAction(SKAction.sequence([actionMove, actionMoveDone]))
+    }
+    
+    private func projectileOffsetFor(touchLocation: CGPoint, projectilePosition: CGPoint) -> CGPoint? {
+        let offset = touchLocation - projectilePosition
+        return offset.x > -10 ? offset : nil
+    }
+    
     private func addPlayer() {
         player = SKSpriteNode(imageNamed: "player")
         player.position = CGPoint(x: size.width * 0.1, y: size.height * 0.5)
